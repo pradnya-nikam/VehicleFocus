bda9a9a49d1561592a3d06afd3390849cfe4ef8e
pradnya <pradnyanikam@gmail.com>
2024-01-05 08:53:03 +0800

    Add different images of cars

--- CarsTV/Vehicle.swift
+++ CarsTV/Vehicle.swift
@@ -10,18 +10,18 @@ struct Vehicle {
   let name: String
   let imageName: String
   public static let cars = [
-    Vehicle(name: "Car", imageName: "car1"),
-    Vehicle(name: "Jeep", imageName: "car1"),
-    Vehicle(name: "Jeep1", imageName: "car1"),
-    Vehicle(name: "Jeep2", imageName: "car1"),
-    Vehicle(name: "Jeep3", imageName: "car1"),
-    Vehicle(name: "Jeep4", imageName: "car1"),
-    Vehicle(name: "Jeep5", imageName: "car1"),
-    Vehicle(name: "Jeep6", imageName: "car1"),
-    Vehicle(name: "Jeep7", imageName: "car1"),
-    Vehicle(name: "Jeep8", imageName: "car1"),
-    Vehicle(name: "Jeep9", imageName: "car1"),
-    Vehicle(name: "Jeep10", imageName: "car1")
+    Vehicle(name: "Car1", imageName: "car1"),
+    Vehicle(name: "Jeep", imageName: "car2"),
+    Vehicle(name: "Jeep1", imageName: "car3"),
+    Vehicle(name: "Jeep2", imageName: "car4"),
+    Vehicle(name: "Jeep3", imageName: "car5"),
+    Vehicle(name: "Jeep4", imageName: "car6"),
+    Vehicle(name: "Jeep5", imageName: "car7"),
+    Vehicle(name: "Jeep6", imageName: "car8"),
+    Vehicle(name: "Jeep7", imageName: "car10"),
+//    Vehicle(name: "Jeep8", imageName: "car10")
+//    Vehicle(name: "Jeep9", imageName: "car1"),
+//    Vehicle(name: "Jeep10", imageName: "car1")
   ]
 
   public static let grid: [(String, [Vehicle])] = [
